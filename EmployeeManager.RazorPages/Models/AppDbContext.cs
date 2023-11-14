2916fa1324a8bf563785a703edbbd3422eb6942a
Mohamed Amine Chafia <chafia.amine1@gmail.com>
2023-11-13 22:35:10 -0500

    Using Data Annotations instead of Fluent API

--- EmployeeManager.RazorPages/Models/AppDbContext.cs
+++ EmployeeManager.RazorPages/Models/AppDbContext.cs
@@ -29,64 +29,13 @@ namespace EmployeeManager.RazorPages.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Countries>(entity =>
-            {
-                entity.HasKey(e => e.CountryId);
-
-                entity.Property(e => e.CountryId).HasColumnName("CountryID");
-
-                entity.Property(e => e.Name)
-                    .IsRequired()
-                    .HasMaxLength(80);
-            });
-
             modelBuilder.Entity<Employees>(entity =>
             {
-                entity.HasKey(e => e.EmployeeId);
-
                 entity.HasIndex(e => e.LastName)
                     .HasName("LastName");
 
                 entity.HasIndex(e => e.PostalCode)
                     .HasName("PostalCode");
-
-                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
-
-                entity.Property(e => e.Address).HasMaxLength(60);
-
-                entity.Property(e => e.BirthDate).HasColumnType("datetime");
-
-                entity.Property(e => e.City).HasMaxLength(15);
-
-                entity.Property(e => e.Country).HasMaxLength(15);
-
-                entity.Property(e => e.Extension).HasMaxLength(4);
-
-                entity.Property(e => e.FirstName)
-                    .IsRequired()
-                    .HasMaxLength(10);
-
-                entity.Property(e => e.HireDate).HasColumnType("datetime");
-
-                entity.Property(e => e.HomePhone).HasMaxLength(24);
-
-                entity.Property(e => e.LastName)
-                    .IsRequired()
-                    .HasMaxLength(20);
-
-                entity.Property(e => e.Notes).HasColumnType("ntext");
-
-                entity.Property(e => e.Photo).HasColumnType("image");
-
-                entity.Property(e => e.PhotoPath).HasMaxLength(255);
-
-                entity.Property(e => e.PostalCode).HasMaxLength(10);
-
-                entity.Property(e => e.Region).HasMaxLength(15);
-
-                entity.Property(e => e.Title).HasMaxLength(30);
-
-                entity.Property(e => e.TitleOfCourtesy).HasMaxLength(25);
 
                 entity.HasOne(d => d.ReportsToNavigation)
                     .WithMany(p => p.InverseReportsToNavigation)
--- EmployeeManager.RazorPages/Models/Countries.cs
+++ EmployeeManager.RazorPages/Models/Countries.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeeManager.RazorPages.Models
 {
     public partial class Countries
     {
+        [Key]
+        [Column("CountryID")]
         public int CountryId { get; set; }
+        [Required]
+        [StringLength(80)]
         public string Name { get; set; }
     }
 }
