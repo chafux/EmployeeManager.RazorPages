4961fe4dd36d996c42c8c03d0d9501a426ba4669
Mohamed Amine Chafia <chafia.amine1@gmail.com>
2023-11-14 21:37:47 -0500

    Create Pages and  EmployeeManager Folders

--- EmployeeManager.RazorPages/Models/AppDbContext.cs
+++ EmployeeManager.RazorPages/Models/AppDbContext.cs
@@ -1,51 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+
+
 
 namespace EmployeeManager.RazorPages.Models
 {
-    public partial class AppDbContext : DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext()
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+
         }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
 
-        public virtual DbSet<Countries> Countries { get; set; }
-        public virtual DbSet<Employees> Employees { get; set; }
+        public DbSet<Employee> Employees { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True");
-            }
-        }
+        public DbSet<Country> Countries { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Employees>(entity =>
-            {
-                entity.HasIndex(e => e.LastName)
-                    .HasName("LastName");
-
-                entity.HasIndex(e => e.PostalCode)
-                    .HasName("PostalCode");
-
-                entity.HasOne(d => d.ReportsToNavigation)
-                    .WithMany(p => p.InverseReportsToNavigation)
-                    .HasForeignKey(d => d.ReportsTo)
-                    .HasConstraintName("FK_Employees_Employees");
-            });
-
-            OnModelCreatingPartial(modelBuilder);
-        }
-
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
---
+++ EmployeeManager.RazorPages/Pages/EmployeeManager/List.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManager.RazorPages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmployeeManager.RazorPages.Pages
+{
+    public class ListModel : PageModel
+    {
+        private readonly AppDbContext db = null;
+        
+        public List<Employee> Employees { get; set; }
+
+        public ListModel(AppDbContext db)
+        {
+            this.db = db;
+        }
+        public void OnGet()
+        {
+            this.Employees = (from e in db.Employees orderby e.EmployeeID select e).ToList();
+        }
+    }
+}
